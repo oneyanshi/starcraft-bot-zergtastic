86cacbffdfe22f71123bf00416989fc3949c5c33
oneyanshi <ys7615a@student.american.edu>
2016-12-01 08:31:18 -0500

    Reverted back to version 1 with mineral gathering because of errors

--- src/java/bot/MinimalAIClient.java
+++ src/java/bot/MinimalAIClient.java
@@ -1,40 +1,87 @@
 package bot;
 
-/**
- * Example of a Java AI Client that does nothing.
- * Where we start coding our bots.
- */
+import java.util.HashSet;
+
 import jnibwapi.BWAPIEventListener;
 import jnibwapi.JNIBWAPI;
 import jnibwapi.Position;
 import jnibwapi.Unit;
-import jnibwapi.util.BWColor;
+import jnibwapi.types.TechType;
+import jnibwapi.types.TechType.TechTypes;
+import jnibwapi.types.UnitType;
+import jnibwapi.types.UnitType.UnitTypes;
+import jnibwapi.types.UpgradeType;
+import jnibwapi.types.UpgradeType.UpgradeTypes;
 
 public class MinimalAIClient implements BWAPIEventListener {
-	private JNIBWAPI bwapi;
-	
+	private final JNIBWAPI bwapi;
+
+	/** used for mineral splits*/
+	private final HashSet<Unit> claimedMinerals = new HashSet<>();
+
+	/**when should the next overlord be spawned?*/
+	private int supplyCap;
+
+	/**the drone that has been assigned to building a pool*/
+	private Unit poolDrone;
+
+	/**have drone 5 been morphed*/
+	private boolean morphedDrone;
+
+	/** Create a Java AI */
 	public static void main(String[] args) {
 		new MinimalAIClient();
 	}
-	
+
 	public MinimalAIClient() {
 		bwapi = new JNIBWAPI(this, false);
 		bwapi.start();
 	}
-	
+
 	@Override
-	public void connected() {}
-	
+	public void connected() { System.out.println("Connected!");}
+
 	@Override
-	public void matchStart() {}
-	
+	public void matchStart() {
+		System.out.println("Game Started");
+
+		bwapi.enableUserInput();
+		bwapi.enablePerfectInformation();
+		bwapi.setGameSpeed(0);
+
+		//reset agent state
+		claimedMinerals.clear();
+
+
+	}
+
 	@Override
 	public void matchFrame() {
-		for (Unit u : bwapi.getAllUnits()) {
-			bwapi.drawCircle(u.getPosition(), 5, BWColor.Red, true, false);
+		// Collect minerals
+		for(Unit unit : bwapi.getMyUnits()) {
+			if(unit.getType() == UnitTypes.Zerg_Drone) {
+				for(Unit minerals : bwapi.getNeutralUnits()) {
+					if(minerals.getType().isMineralField() &&
+							!claimedMinerals.contains(minerals)) {
+						double distance = unit.getDistance(minerals);
+
+						if(distance < 300) {
+							unit.rightClick(minerals, false);
+							claimedMinerals.add(minerals);
+							break;
+
+						}
+					}
+				}
+			}
 		}
+
+			/** If I want to figure out how to place spawning pools,
+			 * I would put them where? Right next to the minerals? A few feet
+			 * away? Look it up later. */
+
 	}
-	
+
 	@Override
 	public void keyPressed(int keyCode) {}
 	@Override
